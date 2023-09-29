fdc43e1bb2339b8ff57177d434b39b4948275232
YouhuiYe-vt <yye1997@vt.edu>
2023-09-29 13:38:04 -0400

    changed the model

--- models.py
+++ models.py
@@ -292,3 +292,46 @@ class VGG16(nn.Module):
         else:
             logits = self.main(x)
         return logits
+
+# Define the neural network class
+class NN(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(NN, self).__init__()
+        self.layer1 = nn.Linear(input_dim, input_dim * 2)
+        self.layer2 = nn.Linear(input_dim * 2, input_dim)
+        self.layer3 = nn.Linear(input_dim, output_dim)
+
+    def forward(self, x):
+        x = torch.relu(self.layer1(x))
+        x = torch.relu(self.layer2(x))
+        x = torch.relu(self.layer3(x))
+        x = torch.sigmoid(x)
+        return x
+    
+    
+class LeNet5(nn.Module):
+    def __init__(self, num_classes):
+        super(LeNet5, self).__init__()
+        self.feature = nn.Sequential(
+            #1
+            nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, stride=1, padding=2),   # 28*28->32*32-->28*28
+            nn.Tanh(),
+            nn.AvgPool2d(kernel_size=2, stride=2),  # 14*14
+            
+            #2
+            nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1),  # 10*10
+            nn.Tanh(),
+            nn.AvgPool2d(kernel_size=2, stride=2),  # 5*5
+            
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features=16*5*5, out_features=120),
+            nn.Tanh(),
+            nn.Linear(in_features=120, out_features=84),
+            nn.Tanh(),
+            nn.Linear(in_features=84, out_features=num_classes),
+        )
+        
+    def forward(self, x):
+        return self.classifier(self.feature(x))
